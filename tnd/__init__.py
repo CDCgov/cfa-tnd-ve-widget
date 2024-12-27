219740b98ea3d87ad91c63b67b27fc095a981024
Scott Olesen <ulp7@cdc.gov>
2024-12-27 17:16:11 -0500

    improve docs

--- tnd/__init__.py
+++ tnd/__init__.py
@@ -1,20 +1,34 @@
-def ev_rr_estimator(eps_v, eps_u, lam_v, lam_u, mu_vi, mu_ui, mu_vx, mu_ux, sens, spec):
+import numpy as np
+
+
+def ev_rr_estimator(
+    eps_v: np.ndarray,
+    eps_u: np.ndarray,
+    lam_v: np.ndarray,
+    lam_u: np.ndarray,
+    mu_vi: np.ndarray,
+    mu_ui: np.ndarray,
+    mu_vx: np.ndarray,
+    mu_ux: np.ndarray,
+    sens: np.ndarray,
+    spec: np.ndarray,
+) -> np.ndarray:
     """Expected value of risk ratio estimator
 
     Args:
-        eps_v (_type_): _description_
-        eps_u (_type_): _description_
-        lam_v (_type_): _description_
-        lam_u (_type_): _description_
-        mu_vi (_type_): _description_
-        mu_ui (_type_): _description_
-        mu_vx (_type_): _description_
-        mu_ux (_type_): _description_
-        sens (_type_): _description_
-        spec (_type_): _description_
+        eps_v (np.ndarray): exposure probability among the vaccinated
+        eps_u (np.ndarray): exposure probability among the unvaccinated
+        lam_v (np.ndarray): infection probability among the exposed vaccinated
+        lam_u (np.ndarray): infection probability among the exposed unvaccinated
+        mu_vi (np.ndarray): receive test probability among the infected vaccinated
+        mu_ui (np.ndarray): receive test probability among the infected unvaccinated
+        mu_vx (np.ndarray): receive test probability among the uninfected vaccinated
+        mu_ux (np.ndarray): receive test probability among the uninfected unvaccinated
+        sens (np.ndarray): test sensitivity
+        spec (np.ndarray): test specificity
 
     Returns:
-        _type_: _description_
+        np.ndarray: expected value
     """
     return 1.0 - (
         eps_v * lam_v * mu_vi * sens + (1.0 - eps_v * lam_v) * mu_vx * (1.0 - spec)
